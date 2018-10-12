2bc61fbc1a6a55f29677b275f0757a9f0cf99ad3
Conrad Haupt <conradjhaupt@gmail.com>
2018-10-12 11:43:31 +0200

    Added tests for sfc::range

---
+++ libsfc/tests/unit_tests/include/range_test.h
@@ -0,0 +1,23 @@
+/**
+ * @file range.h
+ * @author Conrad Haupt
+ * @brief
+ * @version 0.1
+ * @date 2018-10-04
+ *
+ * @copyright Copyright (c) 2018
+ *
+ */
+
+#ifndef SFC_RANGE_TEST_H
+#define SFC_RANGE_TEST_H
+#include <gtest/gtest.h>
+#include "libsfc/range.h"
+
+class range_test_case
+    : public ::testing::TestWithParam<std::tuple<sfc::size_t, sfc::size_t>> {
+ public:
+  sfc::size_t begin = std::get<0>(GetParam());
+  sfc::size_t end = std::get<1>(GetParam());
+};
+#endif
