8033eeafdecf6a09d32b675339196f31fe48cacd
Conrad Haupt <conradjhaupt@gmail.com>
2018-10-12 13:20:10 +0200

    Added sfccompare code for Hilbert and Gray curves

--- sfccompare/src/main.cpp
+++ sfccompare/src/main.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <fstream>
+#include "libsfc/gray.h"
+#include "libsfc/hilbert.h"
 #include "libsfc/morton.h"
 #include "libsfc/range.h"
 
@@ -10,6 +12,8 @@ int main(void) {
   std::cout << std::endl;
   constexpr ::sfc::size_t DIM_SIZE = 1UL << 4;
   constexpr ::sfc::size_t DIM_COUNT = 2;
+
+  // MORTON
   sfc::morton<DIM_COUNT> mrtn(DIM_SIZE);
   std::ofstream mortonFile;
   mortonFile.open("./morton.coords");
@@ -28,5 +32,50 @@ int main(void) {
     mortonFile << coords[0] << "\t" << coords[1] << std::endl;
   }
   mortonFile.close();
+
+  // HILBERT
+  sfc::hilbert<DIM_COUNT> hlbrt(DIM_SIZE);
+  std::ofstream hilbertFile;
+  hilbertFile.open("./hilbert.coords");
+  rows = 0;
+  newline = 0;
+  for (auto dist : sfc::range<sfc::size_t>(DIM_SIZE * DIM_SIZE)) {
+    auto coords = hlbrt.distToCoords(dist);
+    if ((dist / DIM_SIZE) != rows) {
+      rows = dist / DIM_SIZE;
+      std::cout << ".";
+    }
+    if ((dist / (DIM_SIZE * 16)) != newline) {
+      newline = dist / (DIM_SIZE * 16);
+      std::cout << newline << std::endl;
+    }
+    hilbertFile << coords[0] << "\t" << coords[1] << std::endl;
+  }
+  hilbertFile.close();
+
+  // GRAY
+  sfc::gray<DIM_COUNT> gry(DIM_SIZE);
+  std::ofstream grayFile;
+  grayFile.open("./gray.coords");
+  rows = 0;
+  newline = 0;
+  for (auto dist : sfc::range<sfc::size_t>(DIM_SIZE * DIM_SIZE)) {
+    auto coords = gry.distToCoords(dist);
+    if ((dist / DIM_SIZE) != rows) {
+      rows = dist / DIM_SIZE;
+      std::cout << ".";
+    }
+    if ((dist / (DIM_SIZE * 16)) != newline) {
+      newline = dist / (DIM_SIZE * 16);
+      std::cout << newline << std::endl;
+    }
+    grayFile << coords[0] << "\t" << coords[1] << std::endl;
+  }
+  grayFile.close();
+
+  auto mtrc = sfc::bounds_metric<2>(gry);
+  auto mtrc_res = mtrc.calculate();
+  std::cout << std::get<0>(mtrc_res) << " - " << std::get<1>(mtrc_res) << std::endl;
+
   return 0;
 }
