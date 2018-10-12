930c937307ef80811680007a1edbe3878ca2b108
Conrad Haupt <conradjhaupt@gmail.com>
2018-10-12 12:19:08 +0200

    Formatted all CMake files using cmake_format

--- SFCCompare/external/cmake/benchmark-download.cmake
+++ SFCCompare/external/cmake/benchmark-download.cmake
@@ -4,10 +4,13 @@ project(benchmark-downloadproj NONE)
 
 option(BENCHMARK_ENABLE_TESTING "Enable testing of the benchmark library." OFF)
 
-download_project(PROJ                 benchmark
-                 GIT_REPOSITORY       https://github.com/google/benchmark.git
-                 GIT_TAG              master
-                 UPDATE_DISCONNECTED  ON
-                 CMAKE_ARGS           "-DCMAKE_CROSSCOMPILING=ON -DBENCHMARK_ENABLE_TESTING=OFF"
-
-)
+download_project(PROJ
+                 benchmark
+                 GIT_REPOSITORY
+                 https://github.com/google/benchmark.git
+                 GIT_TAG
+                 master
+                 UPDATE_DISCONNECTED
+                 ON
+                 CMAKE_ARGS
+                 "-DCMAKE_CROSSCOMPILING=ON -DBENCHMARK_ENABLE_TESTING=OFF")
