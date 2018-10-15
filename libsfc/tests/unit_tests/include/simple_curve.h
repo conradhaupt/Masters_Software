a3ee4521f93035c6ed6ad22618dc3f4a4001b0d5
Conrad Haupt <conradjhaupt@gmail.com>
2018-10-15 16:08:57 +0200

    Moved simple_curve declaration to its own header for easier inclusion in other test files

---
+++ libsfc/tests/unit_tests/include/simple_curve.h
@@ -0,0 +1,26 @@
+#ifndef SFC_TEST_SIMPLE_CURVE_H
+#define SFC_TEST_SIMPLE_CURVE_H
+
+#include "libsfc/sfcurve.h"
+
+using index_type = typename sfc::sfcurve<2>::index_type;
+using coords_type = typename sfc::sfcurve<2>::coords_type;
+using dist_type = typename sfc::sfcurve<2>::dist_type;
+class simplecurve : public sfc::sfcurve<2>
+{
+ public:
+  using sfc::sfcurve<2>::sfcurve;
+  virtual size_type coordsToDist(const coords_type &coords) const
+  {
+    return sfc::sfcurve<2>::coordsToIndex(coords);
+  };
+  virtual coords_type distToCoords(const dist_type &dist) const
+  {
+    return sfc::sfcurve<2>::indexToCoords(dist);
+  };
+  std::unique_ptr<sfc::sfcurve<2>> clone() const override
+  {
+    return std::make_unique<simplecurve>(*this);
+  }
+};
+#endif
