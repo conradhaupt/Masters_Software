f5e2e85a66fbce4056280c1f20cb216b4708b421
Conrad Haupt <conradjhaupt@gmail.com>
2018-10-16 10:54:05 +0200

    Added new unsigned header with unsigned absolute difference functions

---
+++ SFCCompare/libsfc/include/libsfc/unsigned.h
@@ -0,0 +1,53 @@
+/**
+ * @file unsigned.h
+ * @author Conrad Haupt (conradjhaupt@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2018-10-16
+ *
+ * @copyright Copyright (c) 2018
+ *
+ */
+
+#ifndef SFC_UNSIGNED_H
+#define SFC_UNSIGNED_H
+
+#include <algorithm>
+#include <cmath>
+#include <type_traits>
+namespace sfc {
+
+/**
+ * @brief Returns the absolute difference between two values if their types is
+ * unsigned.
+ *
+ * @tparam _Tp the type for the values supplied
+ * @param t1 the first value
+ * @param t2 the second value
+ * @return the absolute difference between the two values
+ */
+template <typename _Tp>
+inline constexpr auto abs_diff(_Tp t1, _Tp t2)
+    -> std::enable_if_t<std::is_unsigned_v<_Tp>, _Tp>
+{
+  return std::max(t1, t2) - std::min(t1, t2);
+}
+
+/**
+ * @brief Returns the absolute difference between two values if their types is
+ * signed.
+ *
+ * @tparam _Tp the type for the values supplied
+ * @param t1 the first value
+ * @param t2 the second value
+ * @return the absolute difference between the two values
+ */
+template <typename _Tp>
+inline constexpr auto abs_diff(_Tp t1, _Tp t2)
+    -> std::enable_if_t<std::is_signed_v<_Tp>, _Tp>
+{
+  return std::abs(t2 - t1);
+}
+};  // namespace sfc
+
+#endif
