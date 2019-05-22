07ca005148bf4d460a9bfc2254176601137d51b7
Conrad Haupt <conradjhaupt@gmail.com>
2019-05-22 12:51:56 +0200

    Refactor libsfc metric class for reuse on numerous sfcurve objects

--- SFCCompare/libsfc/include/libsfc/metric_clusters.h
+++ SFCCompare/libsfc/include/libsfc/metric_clusters.h
@@ -35,30 +35,27 @@ class clusters_metric
   using nRegions_t = unsigned long;
 
  private:
-  using _metric_inherit = sfc::metric<_NDim, std::map<nClusters_t, nRegions_t>>;
-  const typename sfc::sfcurve<_NDim>::index_type _region_sidelength;
-  using sfcurve_t = typename _metric_inherit::sfcurve_t;
+  using sidelength_t = unsigned long;
+  using index_t = unsigned long long;
+  const sidelength_t _region_sidelength;
 
  public:
-  using typename _metric_inherit::curve_ptr;
-  using metric_t = typename _metric_inherit::metric_t;
+  using metric_inherit = metric<_NDim, std::map<nClusters_t, nRegions_t>>;
+  using metric_t = typename metric_inherit::metric_t;
 
-  template <typename _TpSFC>
-  clusters_metric(const _TpSFC& sfc,
-                  const typename _TpSFC::index_type& region_sidelength)
-      : _metric_inherit::metric(sfc), _region_sidelength(region_sidelength)
+  clusters_metric(const sidelength_t region_sidelength)
+      : _region_sidelength(region_sidelength)
   {
     if (_region_sidelength == 0)
       throw std::invalid_argument(
           "region_sidelength must be a positive integer.");
   }
 
-  virtual metric_t calculate()
+  virtual metric_t calculateFor(const sfc::sfcurve<_NDim>& curve)
   {
     metric_t metric_dist;
     auto region_coord_curve =
-        sfc::raster<_NDim>(_metric_inherit::curve_ptr()->dimensionLength() -
-                           _region_sidelength + 1);
+        sfc::raster<_NDim>(curve.dimensionLength() - _region_sidelength + 1);
 
     const auto region_curve = sfc::raster<_NDim>(_region_sidelength);
 
@@ -70,11 +67,11 @@ class clusters_metric
     for (auto _region_coord = region_coord_curve.begin();
          _region_coord < region_coord_curve.end(); ++_region_coord) {
       pg.addProgress();
-      sfc::clusters<typename sfcurve_t::dist_type> _region_clusters;
+      sfc::clusters<index_t> _region_clusters;
       // For each point in the region
       for (const auto _region_offset : region_curve) {
-        _region_clusters.insert(_metric_inherit::curve_ptr()->coordsToDist(
-            (*_region_coord).coords + _region_offset.coords));
+        _region_clusters.insert(curve.coordsToDist((*_region_coord).coords +
+                                                   _region_offset.coords));
       }
       map_mtx.lock();
       metric_dist[_region_clusters.count()]++;
