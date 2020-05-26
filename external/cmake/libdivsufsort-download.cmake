a250f2a82d2bef4c0dcb9dbc1ae8003ff78ea869
Conrad Haupt <conradjhaupt@gmail.com>
2020-05-26 12:19:50 +0200

    Add libdivsufsort as external dependency

---
+++ external/cmake/libdivsufsort-download.cmake
@@ -0,0 +1,12 @@
+cmake_minimum_required(VERSION 2.8.2)
+
+project(libdivsufsort-downloadproj NONE)
+
+download_project(PROJ
+                 libdivsufsort
+                 GIT_REPOSITORY
+                 https://github.com/y-256/libdivsufsort.git
+                 GIT_TAG
+                 master
+                 UPDATE_DISCONNECTED
+                 ON)
