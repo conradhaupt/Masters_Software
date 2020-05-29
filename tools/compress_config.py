b66682c4cb7c13dd1a92ed9945c068dcf24f9795
Conrad Haupt <conradjhaupt@gmail.com>
2020-05-29 22:01:39 +0200

    Add bitshuffle option to compress_config.py

--- tools/compress_config.py
+++ tools/compress_config.py
@@ -21,7 +21,7 @@ cat invariance_test.csv | \
 
 parallel -j 8 --joblog parallel.compress.log --bar --colsep "\t" \
 ~/Software_Masters/SFCCompare/build/sfccompress/sfccompress \
-compress -s {1} -c {2} {3}
+compress -s {1} -c {2} {3} {4}
 
 """
 
@@ -31,9 +31,10 @@ from itertools import product
 compression_schemes = ['HUFFMAN', 'LZ77', 'LZW',
                        'RLE', 'NONE', 'BWT', 'BZIP_LZ77', 'BZIP_LZW']
 curves = ['ROW_MAJOR', 'MORTON', 'GRAY_CODE', 'HILBERT']
+bitshuffle = ['','-b']
 
 files = sys.argv
 files.remove(sys.argv[0])
 
-for file, curve, comp in product(files, curves, compression_schemes):
-    print('\t'.join([curve, comp, file]))
+for file, curve, comp,bs in product(files, curves, compression_schemes,bitshuffle):
+    print('\t'.join([curve, comp, bs, file]))
