85fa49ada1a017cb4638d6d199f545489a63f483
Conrad Haupt <conradjhaupt@gmail.com>
2020-06-15 23:12:35 +0200

    Update libdivsufsort-download.cmake to use correct DownloadProject formatting

--- external/cmake/libdivsufsort-download.cmake
+++ external/cmake/libdivsufsort-download.cmake
@@ -2,13 +2,9 @@ cmake_minimum_required(VERSION 2.8.2)
 
 project(libdivsufsort-downloadproj NONE)
 
-download_project(PROJ
-                 libdivsufsort
-                 GIT_REPOSITORY
-                 https://github.com/y-256/libdivsufsort.git
-                 GIT_TAG
-                 master
-                 UPDATE_DISCONNECTED
-                 ON
-                 CMAKE_ARGS
-                 "-DBUILD_SHARED_LIBS=OFF")
+download_project(PROJ libdivsufsort
+                 GIT_REPOSITORY https://github.com/y-256/libdivsufsort.git
+                 GIT_TAG master
+                 UPDATE_DISCONNECTED ON
+                 CMAKE_ARGS "-DBUILD_SHARED_LIBS=OFF"
+                 )
