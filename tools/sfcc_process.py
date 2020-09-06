24ce8702ba32a56a4730660ee4d117d4b17ce1cf
Conrad Haupt <conradjhaupt@gmail.com>
2020-09-06 18:51:31 +0200

    Update 'tools' python scripts for updated pipeline scheduling

--- tools/sfcc_process.py
+++ tools/sfcc_process.py
@@ -3,9 +3,13 @@ from sfccfile import SFCC, SFC, Compression, Results, SFCCBuilder, parseFilename
 from os.path import join, basename
 import sys
 from time import time
-from copy import copy
+from copy import deepcopy as copy
 from itertools import product
 from sfcc_ignore import FILENAMES_TO_IGNORE
+
+__data_csv_ska_filename = 'data_ska.csv'
+__data_csv_srtm_filename = 'data_srtm.csv'
+__data_csv_header = 'Bytes,Compression Runtime,Decompression Runtime,Relative Compression Runtime,Relative Decompression Runtime,Name,SFC,Compression,BWT,BitShuffle\n'
 
 
 def isOriginalEntry(entry):
@@ -83,20 +87,25 @@ def process_parallel_log(parallel_filename, add, header=False, print_failed_jobs
             add(sfcc, runtime)
 
 
+def printHelpAndExit():
+    print(
+        'sfcc_process.py <du_results.out> <parallel_compress_log> <parallel_decompress_log>')
+    exit(1)
+
+
 if __name__ == '__main__':
-    if len(sys.argv) != 6:
-        print('sfcc_process.py <du_results.out> <parallel_compress_log> <parallel_compress_cli_log> <parallel_decompress_log> <parallel_decompress_cli_log>')
-        exit(1)
+    if len(sys.argv) != 4:
+        printHelpAndExit()
 
     __print_failed_jobs = False
 
     arg_du_results = sys.argv[1]
     arg_parallel_compress_jobs = sys.argv[2]
-    arg_parallel_compress_normal_jobs = sys.argv[3]
-    arg_parallel_decompress_jobs = sys.argv[4]
-    arg_parallel_decompress_normal_jobs = sys.argv[5]
+    arg_parallel_decompress_jobs = sys.argv[3]
 
     entries = dict()
+
+    sfccs_to_sum = set()
 
     # Load du_results.out data
     print('Loading filesize results')
@@ -109,7 +118,7 @@ if __name__ == '__main__':
             entries[sfcc] = Results(size=size)
 
     # Load runtime from parallel compress jobs
-    print('Loading sfccompress compression runtime results')
+    print('Loading compression runtime results')
     with open(arg_parallel_compress_jobs, 'r') as f_parallel_compress_jobs:
         # Parallel log contains a header line
         _ = f_parallel_compress_jobs.readline()
@@ -129,6 +138,10 @@ if __name__ == '__main__':
 
             sfcc = parseCLI(cli)
 
+            # If the compression scheme is external, make sure to sum the timings later
+            if sfcc.compression.external():
+                sfccs_to_sum.add(sfcc)
+
             if sfcc in entries:
                 # sfcc exists
                 if entries[sfcc].runtime_compress is None:
@@ -140,42 +153,8 @@ if __name__ == '__main__':
                 raise RuntimeError(
                     "Found an unexpected SFCC: %s" % (sfcc), cli)
 
-    # Load runtime from parallel compress jobs using external tools
-    sfccs_to_sum = list()
-    print('Loading external tool compression runtime results')
-    with open(arg_parallel_compress_normal_jobs, 'r') as f_parallel_compress_normal_jobs:
-        # Parallel log contains a header line
-        _ = f_parallel_compress_normal_jobs.readline()
-        lines = f_parallel_compress_normal_jobs.readlines()
-        for line in lines:
-            components = line.strip().split('\t')
-            job_number = components[0]
-            runtime = components[3]
-            exitval = components[6]
-            signal = components[7]
-            cli = components[8]
-            # Log failed jobs
-            if exitval != '0' or signal != '0':
-                if __print_failed_jobs:
-                    print('Found failed job no. %s' % (job_number))
-                continue
-
-            sfcc = parseCLI(cli)
-
-            if sfcc in entries:
-                # sfcc exists
-                if entries[sfcc].runtime_compress is None:
-                    entries[sfcc].runtime_compress = int(float(runtime) * 1000)
-                    sfccs_to_sum.append(sfcc)
-                else:
-                    raise RuntimeError(
-                        'Already existing compression runtime: %s' % (cli))
-            else:
-                raise RuntimeError(
-                    "Found an unexpected SFCC: [%s]: %s" % (sfcc, cli))
-
         # Load runtime from parallel decompress jobs
-    print('Loading sfccompress decompression runtime results')
+    print('Loading decompression runtime results')
     with open(arg_parallel_decompress_jobs, 'r') as f_parallel_decompress_jobs:
         # Parallel log contains a header line
         _ = f_parallel_decompress_jobs.readline()
@@ -195,6 +174,9 @@ if __name__ == '__main__':
 
             sfcc = parseCLI(cli)
 
+            if sfcc.compression.external():
+                sfccs_to_sum.add(sfcc)
+
             if sfcc in entries:
                 # sfcc exists
                 if entries[sfcc].runtime_decompress is None:
@@ -206,40 +188,9 @@ if __name__ == '__main__':
             else:
                 raise RuntimeError("Found an unexpected SFCC: %s" % (sfcc))
 
-    # Load runtime from parallel compress jobs using external tools
-    sfccs_to_sum = list()
-    print('Loading external tool decompression runtime results')
-    with open(arg_parallel_decompress_normal_jobs, 'r') as f_parallel_decompress_normal_jobs:
-        # Parallel log contains a header line
-        _ = f_parallel_decompress_normal_jobs.readline()
-        lines = f_parallel_decompress_normal_jobs.readlines()
-        for line in lines:
-            components = line.strip().split('\t')
-            job_number = components[0]
-            runtime = components[3]
-            exitval = components[6]
-            signal = components[7]
-            cli = components[8]
-            # Log failed jobs
-            if exitval != '0' or signal != '0':
-                if __print_failed_jobs:
-                    print('Found failed job no. %s' % (job_number))
-                continue
-
-            sfcc = parseCLI(cli)
-
-            if sfcc in entries:
-                # sfcc exists
-                if entries[sfcc].runtime_decompress is None:
-                    entries[sfcc].runtime_decompress = int(
-                        float(runtime) * 1000)
-                    sfccs_to_sum.append(sfcc)
-                else:
-                    raise RuntimeError(
-                        'Already existing decompression runtime: %s' % (cli))
-            else:
-                raise RuntimeError(
-                    "Found an unexpected SFCC: [%s]: %s" % (sfcc, cli))
+    print('Removing files to ignore')
+    entries = dict([(e, r) for e, r in entries.items()
+                    if e.filename not in FILENAMES_TO_IGNORE])
 
     # Check if all SFCC files have a runtime and size value
     found_incomplete_sfcc = False
@@ -247,12 +198,17 @@ if __name__ == '__main__':
         if (results.runtime_compress is None or results.runtime_decompress is None or results.size is None) and not isOriginalEntry(sfcc):
             print(sfcc, results, isOriginalEntry(sfcc))
             found_incomplete_sfcc = True
-    if found_incomplete_sfcc:
-        exit(1)
+    # if found_incomplete_sfcc:
+    #     exit(1)
+
+    print('Removing files to ignore')
+    entries = dict([(e, r) for e, r in entries.items()
+                    if e.filename not in FILENAMES_TO_IGNORE])
 
     #############################################
     ##  COMBINE INTERNAL AND EXTERNAL RUNTIME  ##
     #############################################
+    print('Combining external times with internal times')
     for sfcc in sfccs_to_sum:
         if sfcc not in entries:
             raise RuntimeError("Cannot find SFCC in entries: %s" % (sfcc))
@@ -264,28 +220,50 @@ if __name__ == '__main__':
                 "Cannot find original SFCC in entries: %s" % (original_sfcc))
 
         original_results = entries[original_sfcc]
-        entries[sfcc].runtime_compress += original_results.runtime_compress
-        entries[sfcc].runtime_decompress += original_results.runtime_decompress
+        if entries[sfcc].runtime_compress is not None and original_results.runtime_compress is not None:
+            entries[sfcc].runtime_compress += original_results.runtime_compress
+        if entries[sfcc].runtime_decompress is not None and original_results.runtime_decompress is not None:
+            entries[sfcc].runtime_decompress += original_results.runtime_decompress
 
-    with open('data_ska.csv', 'w') as csv:
-        csv.write(
-            'bytes,runtime_compress,runtime_decompress,name,curve,compression,bwt,bitshuffle\n')
+    ###################################
+    ##  CALCULATE RELATIVE RUNTIMES  ##
+    ###################################
+    print('Calculating relative runtimes')
+    for sfcc, results in entries.items():
+        # Get the input sfcc
+        input_sfcc = copy(sfcc)
+        if sfcc.compression != Compression.none:
+            input_sfcc.compression = Compression.none
+        else:
+            input_sfcc.sfc = SFC.raster
+            input_sfcc.bwt = False
+            input_sfcc.bitshuffle = False
+
+        input_results = entries[input_sfcc]
+        # Calculate relative runtime
+        results.relative_runtime_compress = results.runtime_compress - \
+            input_results.runtime_compress
+        results.relative_runtime_decompress = results.runtime_decompress - \
+            input_results.runtime_decompress
+
+        # Reassign entries results
+        entries[sfcc] = results
+
+    with open(__data_csv_ska_filename, 'w') as csv:
+        csv.write(__data_csv_header)
         for sfcc, results in entries.items():
             if '1518913830' in sfcc.filename:
                 csv.write(
-                    ','.join([str(x) for x in [results.size, results.runtime_compress, results.runtime_decompress, sfcc.filename, SFCToStr(sfcc.sfc), CompressionToStr(sfcc.compression), sfcc.bwt, sfcc.bitshuffle]]) + '\n')
+                    ','.join([str(x) for x in [results.to_csv(), sfcc.filename, SFCToStr(sfcc.sfc), CompressionToStr(sfcc.compression), sfcc.bwt, sfcc.bitshuffle]]) + '\n')
 
-    print('Removing files to ignore')
-    entries = dict([(e, r) for e, r in entries.items()
-                    if e.filename not in FILENAMES_TO_IGNORE])
-
-    with open('data_srtm.csv', 'w') as csv:
-        csv.write(
-            'bytes,runtime_compress,runtime_decompress,name,curve,compression,bwt,bitshuffle\n')
+    with open(__data_csv_srtm_filename, 'w') as csv:
+        csv.write(__data_csv_header)
         for sfcc, results in entries.items():
+            if results.isIncomplete():
+                print(f"Results from {sfcc} are incomplete: {results}")
             if '1518913830' not in sfcc.filename:
                 csv.write(
-                    ','.join([str(x) for x in [results.size, results.runtime_compress, results.runtime_decompress, sfcc.filename, SFCToStr(sfcc.sfc), CompressionToStr(sfcc.compression), sfcc.bwt, sfcc.bitshuffle]]) + '\n')
+                    ','.join([str(x) for x in [results.to_csv(), sfcc.filename, SFCToStr(sfcc.sfc), CompressionToStr(sfcc.compression), sfcc.bwt, sfcc.bitshuffle]]) + '\n')
 
     filenames, curves, compression, bitshuffle, bwt, configs = summarise_configs(
         entries)
