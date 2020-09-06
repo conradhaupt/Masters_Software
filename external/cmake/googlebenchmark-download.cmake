74aaf866d448fd48bbcbe43bde50cc57cca97314
Conrad Haupt <conradjhaupt@gmail.com>
2020-09-06 18:49:34 +0200

    Add Google Benchmark library as an external dependency

---
+++ external/cmake/googlebenchmark-download.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 2.8.2)
+
+project(googlebenchmark-downloadproj NONE)
+
+download_project(
+  PROJ
+  googlebenchmark
+  GIT_REPOSITORY
+  https://github.com/google/benchmark.git
+  GIT_TAG
+  master
+  UPDATE_DISCONNECTED
+  ON)
+
+# Prevent GoogleTest from overriding our compiler/linker options when building
+# with Visual Studio
+# set(gtest_force_shared_crt
+#     ON
+#     CACHE BOOL "" FORCE)
